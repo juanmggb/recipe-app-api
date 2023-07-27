326681fb856ef017859c2c526653cd892844ba5a
JuanM <58197473+juanmggb@users.noreply.github.com>
2023-07-27 00:05:51 -0600

    sdfwsfgwerg

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,15 @@
-from drf_spectacular.views import SpectacularAPi
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+
+
 from django.contrib import admin
 from django.urls import path
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
 ]
