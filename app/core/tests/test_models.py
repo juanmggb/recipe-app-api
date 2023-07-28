06cab2bd84ed307c43298551dfb07c739fda025d
JuanM <58197473+juanmggb@users.noreply.github.com>
2023-07-27 21:04:08 -0600

    A lot of stuff here

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,9 +2,16 @@
 Test for models
 """
 
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def create_user(email="user@example.com", password="testpass123"):
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTest(TestCase):
@@ -43,3 +50,33 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpass123",
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe Description",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        user = create_user()
+
+        ingredient = models.Ingredient.objects.create(user=user, name="ingredient")
+
+        self.assertEqual(str(ingredient), ingredient.name)
